642238b10a544703cfa207ef4ba094c822d507fc
ringochu <ringo.chu.16@ucl.ac.uk>
2018-07-05 14:46:47 +0100

    none

---
+++ PTWebApp/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBVVlS8iN5Y6EzTHepACgp80ntmWxxWNrk",
+  authDomain: "uclupasssystem.firebaseapp.com",
+  databaseURL: "https://uclupasssystem.firebaseio.com",
+  projectId: "uclupasssystem",
+  storageBucket: "uclupasssystem.appspot.com",
+  messagingSenderId: "903313339253"
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppNavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // Add this
+    AngularFirestoreModule,
+    FormsModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
