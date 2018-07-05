a6334b018daded9673d5a37ec065ef6cb8030386
ringochu <ringo.chu.16@ucl.ac.uk>
2018-07-05 16:41:04 +0100

    Please

---
+++ PartTimeMobileApp/src/pages/tabs/tabs.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { ListPage } from '../list/list';
+/**
+ * Generated class for the TabsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+
+  tab1Root: any = HomePage;
+  tab2Root: any = ListPage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TabsPage');
+  }
+
+}
