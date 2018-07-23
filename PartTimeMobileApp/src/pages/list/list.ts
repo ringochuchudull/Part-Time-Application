59f68a19132e1c124099b0b698995340967285b1
Ringo <zcabswc@ucl.ac.uk>
2018-07-23 02:55:20 +0100

    update rh-tab, ching = shite

--- PartTimeMobileApp/src/pages/list/list.ts
+++ PartTimeMobileApp/src/pages/list/list.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController  } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
 import { Post } from '../../models/post.interface';
 import { FirestoreProvider } from '../../providers/firestore/firestore';
 import { Observable } from 'rxjs/Observable';
 import { DetailPage } from '../detail/detail';
+import { AngularFirestore } from 'angularfire2/firestore'
 
 @Component({
   selector: 'page-list',
@@ -15,13 +16,21 @@ export class ListPage {
 
   sectionTitle: string;
   public postList: Observable<Post[]>;
+
+  title:string;
+  region: string;
+  durationtime: string;
+  content:string;
+  left: number = 0;
 //  selectedItem: any;
  // icons: string[];
  // items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController,
+              private toastCtrl: ToastController,
               public storage: Storage,
-              public firestoreProvider: FirestoreProvider
+              public firestoreProvider: FirestoreProvider,
+              private afs: AngularFirestore
               ){
       storage.get('section').then((val) => {
       this.mainOperationRT(val);
@@ -32,12 +41,31 @@ export class ListPage {
     this.postList = this.firestoreProvider.getPostList().valueChanges();
   }
 
+  addPost() {
+    this.afs.collection('posts').doc(this.title).set({'title': this.title, 'content': this.content, 'durationtime':this.durationtime, 'left': this.left, 'type':this.sectionTitle });
+    this.presentToast();
+  }
+
   goToDetailPage(post: Post): void {
     this.navCtrl.push(DetailPage, {post:post} );
   }
 
   mainOperationRT( titlename ){
     this.sectionTitle = titlename
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: '成功上載',
+      duration: 2000,
+      position: 'middle'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
   }
 
 }
